34e79cadf34a5c82ec2c331135e590de2c647436
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-03-04 12:02:56 +0100

    Creato reindirizzamento alla 404

---
+++ resources/js/guest/pages/PageNotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notFound my-5 d-flex justify-content-center">
+    <h1>404 | Page Not Found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "PageNotFound",
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+    @import '../../../sass/front.scss';
+ 
+    .notFound{
+        color: $brownDark;
+    }
+
+</style>
--- resources/js/guest/pages/SinglePost.vue
+++ resources/js/guest/pages/SinglePost.vue
@@ -21,8 +21,12 @@ export default {
     created() {
 
         axios.get(`/api/posts/${this.$route.params.slug}`)
+
             .then( (response) => {
                 this.post = response.data;
+            })
+            .catch( (error) => {
+            this.$router.push({name: 'page-404'});
             });
     }
 }
